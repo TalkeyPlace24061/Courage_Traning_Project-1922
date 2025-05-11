9007b0485b39971388d30ec980711b21840b23cc
ESchindewolf <schindewolf.evan@gmail.com>
2025-05-11 15:34:03 -0400

    LEDs color and Servo Motor angles are now combined into the same variable.

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -11,19 +11,22 @@ import java.util.RandomAccess;
 import edu.wpi.first.hal.LEDJNI;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.LEDWriter;
 import edu.wpi.first.wpilibj.LEDPattern.GradientType;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.commands.usingTheSubsystems;
+import frc.robot.subsystems.ServoMotor;
 
 public class LED extends SubsystemBase {
   private  AddressableLED  m_led;
   private  AddressableLEDBuffer m_ledBuffer;
 
   /** Creates a new LED. */
-  public LED() {
-
+  public LED(usingTheSubsystems usingSubsystems) {
+   ServoMotor m_servo;
 
   m_led = new AddressableLED(0);
   m_ledBuffer = new AddressableLEDBuffer(3);
@@ -35,14 +38,14 @@ public class LED extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
 
-   //LEDPattern base = LEDPattern.gradient(GradientType.kDiscontinuous, Color.kRed, Color.kBlue);
-   //LEDPattern pattern = base.blink(Seconds.of(.5),Seconds.of(.5));
-   //pattern.applyTo(m_ledBuffer);
-   //m_led.setData(m_ledBuffer);
+   /*LEDPattern base = LEDPattern.gradient(GradientType.kDiscontinuous, Color.kRed, Color.kBlue);
+   LEDPattern pattern = base.blink(Seconds.of(.5),Seconds.of(.5));
+   pattern.applyTo(m_ledBuffer);
+   m_led.setData(m_ledBuffer);*/
 
-   //LEDPattern red = LEDPattern.solid(Color.kGreen);
-   //red.applyTo(m_ledBuffer);
-   //m_led.setData(m_ledBuffer);
+   /*LEDPattern red = LEDPattern.solid(Color.kGreen);
+   red.applyTo(m_ledBuffer);
+   m_led.setData(m_ledBuffer);*/
 
    LEDPattern.solid(new Color(90,89,70)).applyTo(m_ledBuffer);
   }
